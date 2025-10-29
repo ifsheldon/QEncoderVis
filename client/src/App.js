f6f84c12c01e0d92b248b1e52f06bbfda296b463
ifsheldon <feng.liang@kaust.edu.sa>
2025-10-29 11:07:14 +0800

    changed server url

--- client/src/App.js
+++ client/src/App.js
@@ -33,7 +33,7 @@ import QuantumStateDistributionView from "./Components/quantum_state_distributio
 import DataFlowLink from "./Components/data_flow_link";
 import DescriptionComp from "./Components/description";
 
-const server_address = "https://xqai-encoder-server.reify.ing";
+const server_address = "https://xqai-encoder-server-backup.reify.ing";
 
 // 布局参数
 const vis_width = 1060;
